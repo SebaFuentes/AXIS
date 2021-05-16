b6750ab548edc7756429bbff73c84b0f803cadc4
WindWaker991 <83779081+WindWaker991@users.noreply.github.com>
2021-05-15 22:36:10 -0400

    destruccion meteoritos
    ahora los meteoritos se destruyen

---
+++ Assets/Scripts/destruccionMateoro.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruccionMateoro : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Meteorito"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/siguiendoJugador.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class siguiendoJugador : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    public float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        temp.x += offset;
+
+        transform.position = temp;
+    }
+}
