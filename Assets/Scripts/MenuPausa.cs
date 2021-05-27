b3b4d0dc5331b7e1e7bada902ad7c3692bfa4c23
WindWaker991 <83779081+WindWaker991@users.noreply.github.com>
2021-05-26 21:38:56 -0400

    Pausar juego (aun en desarrollo)

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -1,42 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class MenuPausa : MonoBehaviour
 {
     private InputMaster controles;
     public static bool JuegoEnPausa = false;
-    public GameObject PauseMenuUI;
-    
-    
-    void Update()
-    {
-        bool pausa = controles.Menu.Menupausa.triggered;
-        if (pausa)
-        {
-            if (JuegoEnPausa)
-            {
-                ResumeGame();
-            }
-            else
-            {
-                PauseGame();
-            }
-        }
-    }
-    
+    [SerializeField]
+    private GameObject CanvasMenu;
 
+    private void Awake()
+    {
+        controles = new InputMaster();
+    }
+
+    private void OnEnable()
+    {
+        controles.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controles.Disable();
+    }
+
+    private void Start()
+    {
+        controles.Menu.Menupausa.performed += _ => Pausa();
+    }
+
+    private void Pausa()
+    {
+        if (JuegoEnPausa)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }    
     private void ResumeGame()
     {
-        PauseMenuUI.SetActive(false);
+        CanvasMenu.SetActive(false);
         Time.timeScale = 1f;
         JuegoEnPausa = false;
     }
 
     private void PauseGame()
     {
-        PauseMenuUI.SetActive(true);
+        CanvasMenu.SetActive(true);
         Time.timeScale = 0f;
         JuegoEnPausa = true;
     }
