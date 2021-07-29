ae3c581d0fd3d0ac105f6f21a1791c0adaf68799
kamila <kamila.opazo@alumnos.ucn.cl>
2021-07-29 16:29:12 -0400

    terminado menu pausa

--- Assets/Escenas AXIS/Menu Opciones/CodigoOpciones/ControladorOpciones.cs
+++ Assets/Escenas AXIS/Menu Opciones/CodigoOpciones/ControladorOpciones.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controladordeopciones : MonoBehaviour
+public class ControladorOpciones : MonoBehaviour
 {
     public GameObject pantallaOpciones;
     // Start is called before the first frame update
